f64b2b398d6ca62bff6e3a9a53cf20dda2b240ff
MgHtetMyatAung <htetmyataung16000@gmail.com>
2023-02-07 03:24:28 +0630

    contact-update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
 
   return (
     <div className='contact bg-black text-white' id='contact'>
-      <div className="ct_body py-20">
+      <div className="ct_body py-24">
         <div className="grid grid-cols-1 md:grid-cols-3">
           <div className="" data-aos="zoom-in">
             <h2 className='text-[20px] font-semibold uppercase'>Contact</h2>
@@ -30,7 +30,7 @@ const Contact = () => {
                 <li>Maintenance</li>
               </ul>
           </div>
-          <div className="flex md:flex-col gap-10 md:gap-3 justify-center md:items-end" data-aos="zoom-in">
+          <div className="flex md:flex-col gap-8 md:gap-3 justify-center md:items-end" data-aos="zoom-in">
             <a href='https://www.facebook.com/profile.php?id=100042798099129&mibextid=ZbWKwL' className="w-[40px] md:w-[200px] h-[40px] bg-white rounded-full flex items-center cursor-pointer text-black hover:text-kayan transition-all duration-200">
               <FaFacebook size={25} className="ml-2 md:ml-3"/>
               <span className='text-black hidden md:block md:ml-3 text-[18px] cursor-pointer'>Facebook</span>
