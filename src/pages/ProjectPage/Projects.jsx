fa4d8f5bdb62061017f1999bf6c126c5070a1cfc
WaiYanHtun <waiyanhtun282@gmail.com>
2025-03-25 20:19:58 +0400

    finish rpofilio9000

--- src/pages/ProjectPage/Projects.jsx
+++ src/pages/ProjectPage/Projects.jsx
@@ -6,53 +6,117 @@ import movieImg from "../../assets/imgs/movie.png";
 import blogImg from "../../assets/imgs/blog.png";
 import tealtalkImg from "../../assets/imgs/teatalk.png";
 import ProjectCard from '../../components/nav/ProjectCard';
+import foodmarketimg from '../../assets/imgs/foodmarket.png';
+import backendimg from '../../assets/imgs/backend.png'
 import "./project.css";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-const projectsData=[
+const projectsData = [
   {
-    id:1,
-    title:"Juice Shopping",
-    image: <img src={juiceImg} alt="" className='w-100 shadow-lg h-auto rounded-[8px]'/>,
+    id: 1,
+    title: "Juice Shopping",
+    image: (
+      <img
+        src={juiceImg}
+        alt=""
+        className="w-100 shadow-lg h-auto rounded-[8px]"
+      />
+    ),
     url: "https://juice-shopping-gamma.vercel.app/",
-    git: "https://github.com/waiyanhtun282/JuiceShopping"
+    git: "https://github.com/waiyanhtun282/JuiceShopping",
   },
   {
-    id:2,
-    title:" Business Website",
-    image: <img src={businessImg} alt="" className='w-100 shadow-lg h-auto rounded-[8px]'/>,
-    url:"https://business-profilo.vercel.app/",
-    git:"https://github.com/waiyanhtun282/BusinessProfilo"
+    id: 2,
+    title: " Business Website",
+    image: (
+      <img
+        src={businessImg}
+        alt=""
+        className="w-100 shadow-lg h-auto rounded-[8px]"
+      />
+    ),
+    url: "https://business-profilo.vercel.app/",
+    git: "https://github.com/waiyanhtun282/BusinessProfilo",
   },
   {
-    id:3,
-    title:"Movie Website",
-    image: <img src={movieImg} alt="" className='w-100 shadow-lg h-auto rounded-[8px]'/>,
-    url:"https://movie-pj-development.vercel.app/",
-    git:"https://github.com/waiyanhtun282/MoviePj"
+    id: 3,
+    title: "Movie Website",
+    image: (
+      <img
+        src={movieImg}
+        alt=""
+        className="w-100 shadow-lg h-auto rounded-[8px]"
+      />
+    ),
+    url: "https://movie-pj-development.vercel.app/",
+    git: "https://github.com/waiyanhtun282/MoviePj",
   },
   {
-    id:4,
-    title:"Mini Shopping",
-    image: <img src={shoppingImg} alt="" className='w-100 shadow-lg h-auto rounded-[8px]'/>,
-    url:"https://swd-redux-mini-shopping.netlify.app/",
-    git:"https://github.com/MgHtetMyatAung/redux-shopping-pj.git"
+    id: 4,
+    title: "Mini Shopping (Collaboration) ",
+    image: (
+      <img
+        src={shoppingImg}
+        alt=""
+        className="w-100 shadow-lg h-auto rounded-[8px]"
+      />
+    ),
+    url: "https://swd-redux-mini-shopping.netlify.app/",
+    git: "https://github.com/waiyanhtun282/EcomerceMIniSHop",
   },
   {
-    id:5,
-    title:"Blog Website",
-    image: <img src={blogImg} alt="" className='w-100 shadow-lg h-auto rounded-[8px]'/>,
-    url:"https://blogin-figma.vercel.app/",
-    git:"https://github.com/waiyanhtun282/bloginFigma"
+    id: 5,
+    title: "Blog Website",
+    image: (
+      <img
+        src={blogImg}
+        alt=""
+        className="w-100 shadow-lg h-auto rounded-[8px]"
+      />
+    ),
+    url: "https://blogin-figma.vercel.app/",
+    git: "https://github.com/waiyanhtun282/bloginFigma",
   },
   {
-    id:6,
-    title:"Collaboration with Tealtalk Chat App ",
-    image: <img src={tealtalkImg} alt="" className='w-100 shadow-lg h-auto rounded-[8px]'/>,
-    url:"https://teatalk.io/",
-    git:""
-  }
+    id: 6,
+    title: "Collaboration with Tealtalk Chat App ",
+    image: (
+      <img
+        src={tealtalkImg}
+        alt=""
+        className="w-100 shadow-lg h-auto rounded-[8px]"
+      />
+    ),
+    url: "https://teatalk.io/",
+    git: "",
+  },
+  {
+    id: 7,
+    title: "Collaboration with Ecommerce  App ",
+    image: (
+      <img
+        src={foodmarketimg}
+        alt=""
+        className="w-100 shadow-lg h-auto rounded-[8px]"
+      />
+    ),
+    url: "https://food-delivery-admin-dashboard.vercel.app/",
+    git: "",
+  },
+  {
+    id: 8,
+    title: "Backend Ecommerce APi ",
+    image: (
+      <img
+        src={backendimg}
+        alt=""
+        className="w-100 shadow-lg h-auto rounded-[8px]"
+      />
+    ),
+    url: "https://food-delivery-admin-dashboard.vercel.app/",
+    git: "https://github.com/waiyanhtun282/ecommerceProject",
+  },
 ];
 
 const Projects = () => {
